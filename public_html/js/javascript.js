d71feffd58466ea2ace4a0046917a98274f98ef3
GabrielaDianecce <gabrieladianecce@gmail.com>
2019-07-01 17:06:06 -0300

    cambiando

--- public_html/js/javascript.js
+++ public_html/js/javascript.js
@@ -12,3 +12,32 @@ function showSlides() {
        slides[slideIndex-1].style.display = "block";
        setTimeout(showSlides,3000);
 }
+
+
+
+var video1 = document.getElementById('video1');
+var video2 = document.getElementById('video2');
+var video3 = document.getElementById('video3');
+var imagen1 = document.getElementById('imagen1');
+
+function imgTransition(){
+    imagen1.style.opacity=0;
+    video1.play();
+    video1.style.opacity=1;
+}
+video1.onended = function(){
+    video2.play();
+    video1.style.opacity=0;
+    video2.style.opacity=1;
+}
+video2.onended = function(){
+    video3.play();
+    video2.style.opacity=0;
+    video3.style.opacity=1;
+}
+video3.onended = function(){
+    video3.style.opacity=0;
+    imagen1.style.opacity=1;
+    window.setTimeout(imgTransition, 5000);
+}
+
